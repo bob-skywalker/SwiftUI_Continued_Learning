7c4065be748edf23813a60f1441751f12c72e5ad
Bob <q463250938@gmail.com>
2024-07-18 22:47:33 -0700

    Initial Commit

---
+++ SwiftUI_Continued_Learning/SwiftUI_Continued_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Continued_LearningApp.swift
+//  SwiftUI_Continued_Learning
+//
+//  Created by Bo Zhong on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Continued_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
